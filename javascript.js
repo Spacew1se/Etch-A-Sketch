cf6ab9a02fb589afe51bf498cdde2fbf57c524cb
Spacew1se <crod201@gmail.com>
2023-07-20 19:47:00 -0700

    Implement flexbox to display grid
    No longer explicity define height/width of each square
    Allows for changing number of squares per side while grid size stays constant
    Rename gridRow class to the more accurate gridColumn

--- javascript.js
+++ javascript.js
@@ -7,21 +7,19 @@ const gridContainer = document.querySelector('.gridContainer');
 function createGrid(gridSize) {
   
     for (let h=0; h<gridSize; h++) {    
-        const gridRow = document.createElement('div');
-        gridRow.classList.add('gridRow');
-        gridContainer.appendChild(gridRow);           
+        const gridColumn = document.createElement('div');
+        gridColumn.classList.add('gridColumn');
+        gridContainer.appendChild(gridColumn);           
     }       
     
-    const rows = document.querySelectorAll('.gridRow');
+    const rows = document.querySelectorAll('.gridColumn');
     rows.forEach(row => {
         for (let w=0; w <gridSize; w++) {
             const gridSquare = document.createElement('div');
             gridSquare.classList.add('gridSquare');
             row.appendChild(gridSquare);
         }
-    })
-    
-    
+    }) 
 }
 
 createGrid(gridSize);
